4dcb36aa89dd1adaf761801fbabc9e278ad986c6
Josh Taylor <thie.common.coder@gmail.com>
2024-05-26 12:25:33 -0700

    make changes for github pages

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 setTimeout(() => {
   const p = document.createElement("p");
-  p.innerText = "this was added dynamically";
+  p.innerText = "and it's dynamic, too!";
   document.body.appendChild(p);
 }, 2000);
